9a1bc9498fc0be26cf8d7e51c88cdaa9fa69adca
ays-tech <54658247+ays-tech@users.noreply.github.com>
2024-11-26 16:18:52 +0000

    newsk

--- app/src/config.ts
+++ app/src/config.ts
@@ -79,10 +79,6 @@ export default defineConfig({
           button: 'Next',
         },
 
-
-
-
-        
        
         // video
         {
@@ -109,7 +105,7 @@ export default defineConfig({
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Lists',
+          title: 'lets connect',
           description:
             'Stay connected with us on: <br> ' +
             '<a href="https://x.com/NANEEpump_SOL" target="_blank">🐦 Twitter</a>, ' +
@@ -143,34 +139,7 @@ export default defineConfig({
           ],
           button: 'Next',
         },
-        
 
-       
-        
-        // slide with other features
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_cool.tgs'),
-            size: 150,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Some other features:',
-          description: '',
-          list: [
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-            "And many more... (see <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>GitHub</a>)",
-          ],
-          button: 'Next',
-        },
-
-        // go to paywall slide
         {
           media: {
             type: 'sticker',
@@ -180,70 +149,15 @@ export default defineConfig({
           shape: 'square',
           pagination: 'count',
           textAlign: 'center',
-          title: 'But onboarding slides are not enough...',
-          description: "Let's go to Paywall",
-          button: {
-            content: 'Go to Paywall',
-            to: '/paywall',
-          },
-        },
+          title: 'Big Things Are Coming Soon... 🚀',
+          description: 
+            "Exciting things are on the way for our community! 🎉<br><br>" + 
+            "We're gearing up to launch a mini app based entirely on the NANEE ecosystem. 🌟<br><br>" +
+            "Stay tuned and follow our handles to be the first to know about updates and exclusive features! 🚀",
+            button: 'Next',        },
+        
       ],
     },
-
-    // paywall
-    {
-      extends: 'paywall',
-      path: '/paywall',
-      media: {
-        type: 'sticker',
-        src: import('./assets/stickers/duck_money.tgs'),
-        size: 150,
-      },
-      shape: 'square',
-      title: 'Your beautiful Paywall',
-      list: [
-        'Adjustable product cards',
-        '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
-        'Subscriptions or One-time payments',
-      ],
-      products: [
-        {
-          id: '1_month_subscription',
-          title: '1 month subscription',
-          description: '2$/month',
-          discount: '',
-          price: 2,
-        },
-        {
-          id: '1_year_subscription',
-          title: '1 year subscription',
-          description: '1$/month',
-          discount: 'Discount 50%',
-          price: 12,
-        },
-        {
-          id: 'lifetime_access',
-          title: 'Lifetime access',
-          description: '20$ once',
-          discount: 'Best offer',
-          price: 20,
-        },
-      ],
-      mainButtonText: 'Buy for {price}',
-      popup: {
-        // popup for payment methods choice
-        type: 'web',
-      },
-      links: [
-        {
-          text: 'Privacy policy',
-          href: 'https://google.com',
-        },
-        {
-          text: 'Terms of use',
-          href: 'https://google.com',
-        },
-      ],
-    },
+    
   ],
 });
